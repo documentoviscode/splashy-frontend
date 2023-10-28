1aa30a65948339403b4dfc99028ec57ece171da8
Paweł Cichowski <pcichowski@pcichowski.com>
2023-10-28 23:24:49 +0200

    DC-40 pages: partner: create placeholder page
    Signed-off-by: Paweł Cichowski <pcichowski@pcichowski.com>

---
+++ pages/partner.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="main-container">
+        <span>Podstrona partnera</span>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+.main-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 4em 0;
+    font-family: Lato, Helvetica Neue, Noto Sans, sans-serif;
+}
+
+span {
+    font-size: 2em;
+    font-weight: 500;
+}
+</style>
