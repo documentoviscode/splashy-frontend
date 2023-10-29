50703e744320c58facfab01274d67e3f017f84a8
Paweł Cichowski <pcichowski@pcichowski.com>
2023-10-29 13:37:52 +0100

    DC-40 components: infoCard: create component
    Signed-off-by: Paweł Cichowski <pcichowski@pcichowski.com>

---
+++ components/infoCard.vue
@@ -0,0 +1,57 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+    name: "test",
+    props: {
+        data: String,
+        iconName: {
+            required: true,
+            type: String,
+        },
+        caption: String,
+    }
+});
+
+
+</script>
+
+<template>
+    <div class="container">
+        <div class="top">
+            <span>{{ data }}</span>
+        </div>
+        <div class="bottom">
+            <Icon :name="'ion:' + iconName" size="18"/>
+            <span>{{ caption }}</span>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+
+.container {
+    background-color: $background600;
+    padding: 2em 2.4em;
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+    gap: 0.8em;
+    border-radius: 12px;
+    border: $primary500 0.2px solid;
+    box-shadow: 0px 0px 3px -1px $primary300;
+}
+
+.top {
+    font-size: 1.7em;
+    font-weight: 600;
+}
+
+.bottom {
+    display: flex;
+    align-items: center;
+    gap: 0.4em;
+    font-size: 1.2em;
+}
+
+</style>
