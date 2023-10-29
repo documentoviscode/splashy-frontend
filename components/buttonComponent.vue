2a7a78346592e6b7b529f52a4b83c532b12cc39f
Paweł Cichowski <pcichowski@pcichowski.com>
2023-10-29 15:27:22 +0100

    DC-40 components: buttonComponent: create simple component
    Signed-off-by: Paweł Cichowski <pcichowski@pcichowski.com>

---
+++ components/buttonComponent.vue
@@ -0,0 +1,64 @@
+<script lang="ts">
+    import {defineComponent} from 'vue'
+
+    export default defineComponent({
+        name: "button",
+        props: {
+            iconName: {
+                type: String,
+                required: false,
+            },
+            text: {
+                type: String,
+                required: true,
+            },
+            color: {
+                type: String,
+                default: '#3770dd',
+            },
+            onClick: {
+                type: Function,
+                required: false
+            }
+        },
+    });
+
+</script>
+
+<template>
+    <div class="container" :style="{ backgroundColor: color }" @click="onClick">
+        <Icon class="icon" v-if="iconName" :name="`ion:${iconName}`" size="21"/>
+        <span>{{ text }}</span>
+    </div>
+</template>
+
+<style scoped lang="scss">
+    .container {
+        padding: 0.8em 1.2em;
+        display: inline-flex;
+        align-items: center;
+        border-radius: 20px;
+
+        & > span {
+            font-weight: 700;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+        }
+
+        & > .icon {
+            margin-right: 0.4em;
+        }
+
+        transition: 0.1s ease-in-out;
+
+        &:hover {
+            filter: brightness(0.85);
+        }
+
+        &:active {
+            filter:brightness(0.75);
+        }
+    }
+</style>
