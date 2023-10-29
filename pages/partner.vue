2e90fac4925532410e5db061348a2d055d41c35b
Paweł Cichowski <pcichowski@pcichowski.com>
2023-10-29 16:14:14 +0100

    DC-40 pages: partner: add statistics and footer
    Signed-off-by: Paweł Cichowski <pcichowski@pcichowski.com>

--- pages/partner.vue
+++ pages/partner.vue
@@ -10,7 +10,7 @@
                 nickname="belledelphine97_"
                 full-name="Belle Delphine"
                 profile-picture-file="streamer1.jpg"
-                verified="true"
+                :verified="true"
             />
             <div class="brief-statistics">
                 <info-card
@@ -30,7 +30,7 @@
                 />
             </div>
             <div class="contract">
-                <div class="text">
+                <div class="section-header">
                     <span>Twoja umowa</span>
                 </div>
                 <div class="card">
@@ -59,6 +59,48 @@
                     </div>
                 </div>
             </div>
+            <div class="statistics">
+                <div class="section-header">
+                    <span>Statystyki</span>
+                </div>
+                <div class="card">
+                    <USelectMenu
+                        class="month-select"
+                        v-model="selectedMonth"
+                        :options="months"
+                        size="xl"
+                        color="#3770dd"
+                    />
+
+                    <div class="section">
+                        <span class="caption">Suma godzin oglądalności:</span>
+                        <span class="important">163200 h</span>
+                    </div>
+                    <div class="section">
+                        <span class="caption">Suma dotacji:</span>
+                        <span class="important">84 510 zł</span>
+                    </div>
+                    <div class="section">
+                        <span class="caption">Liczba unikalnych widzów:</span>
+                        <span class="important">23 452</span>
+                    </div>
+                    <div class="section">
+                        <span class="caption">Zarobek za oglądalność:</span>
+                        <span class="important">4 896 zł</span>
+                    </div>
+
+                    <div class="buttons small">
+                        <button-component
+                            :text="`Wygeneruj raport z miesiąca - ${selectedMonth}`"
+                            icon-name="document-text-outline"
+                            color="#18408e"
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer">
+            <span>&copy; Documentovisco 2023</span>
         </div>
     </div>
 </template>
@@ -68,19 +110,20 @@
     const views = ref('297.5 tys.');
     const viewTime = ref('45 mln. h');
 
+    const months = ['Czerwiec 2023', 'Lipiec 2023', 'Sierpień 2023', 'Wrzesień 2023', 'Październik 2023', 'Listopad 2023']
+    const selectedMonth = ref(months.at(-1));
 </script>
 
 <style lang="scss" scoped>
     .main-container {
         display: flex;
         flex-direction: column;
-        padding: 0;
-        margin: 0;
         font-family: Lato, Helvetica Neue, Noto Sans, sans-serif;
     }
     .header {
         display: flex;
         flex: 1;
+        align-items: center;
         background-color: $primary600;
 
         & > img {
@@ -89,7 +132,13 @@
 
         & > h1 {
             padding: 0 1em;
+            font-size: 1.7em;
         }
+    }
+
+    .section-header {
+        font-size: 2em;
+        margin-bottom: 0.6em;
     }
 
     .content {
@@ -99,8 +148,7 @@
         width: 100%;
         align-self: center;
         max-width: 800px;
-        max-height: 800px;
-        padding: 2em 0;
+        padding: 2em 0 4em;
 
         & > span {
             font-size: 1.6em;
@@ -119,40 +167,61 @@
     }
 
     .contract {
-        & > .text {
-            font-size: 2em;
-            margin-bottom: 0.6em;
+
+        margin-bottom: 3.2em;
+    }
+
+    .card {
+        background-color: $background600;
+        border: $primary500 0.2px solid;
+        border-radius: 12px;
+        box-shadow: 4px 4px 12px 0 $background800;
+        padding: 2em;
+        display: flex;
+        flex-direction: column;
+        gap: 0.8em;
+
+        & > .section {
+            display: flex;
+            gap: 0.6em;
+            align-items: flex-end;
+
+            & > .important {
+                font-size: 1.3em;
+                font-weight: 600;
+            }
+
+            & > .caption {
+                font-size: 1.2em;
+            }
         }
 
-        & > .card {
-            background-color: $background600;
-            border: $primary500 0.2px solid;
-            border-radius: 12px;
-            box-shadow: 4px 4px 12px 0 $background800;
-            padding: 2em;
+        & > .buttons {
+            margin-top: 1em;
             display: flex;
-            flex-direction: column;
-            gap: 0.8em;
+            gap: 1em;
+        }
+    }
 
-            & > .section {
-                display: flex;
-                gap: 0.6em;
-                align-items: flex-end;
+    .statistics {
 
-                & > .important {
-                    font-size: 1.6em;
-                }
+    }
 
-                & > .caption {
-                    font-size: 1.3em;
-                }
-            }
+    .month-select {
+        margin-bottom: 0.6em;
+    }
 
-            & > .buttons {
-                margin-top: 1em;
-                display: flex;
-                gap: 1em;
-            }
+    .footer {
+        display: flex;
+        flex-direction: column;
+        flex: 1;
+        background-color: $background600;
+        padding: 2em 4em;
+
+        & > span {
+            align-self: flex-end;
+            font-weight: 300;
+            color: $background300;
         }
     }
 </style>
