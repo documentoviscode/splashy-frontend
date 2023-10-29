c29af444104e2dca335aa0be3e5c2c83e631343d
Paweł Cichowski <pcichowski@pcichowski.com>
2023-10-29 14:45:13 +0100

    DC-40 pages: partner: add first version of contract section
    Signed-off-by: Paweł Cichowski <pcichowski@pcichowski.com>

--- pages/partner.vue
+++ pages/partner.vue
@@ -14,26 +14,51 @@
             />
             <div class="brief-statistics">
                 <info-card
-                    data="297.5 tys."
+                    :data="views"
                     icon-name="person-outline"
                     caption="Wyświetlenia"
                 />
                 <info-card
-                    data="2 316 680 zł"
+                    :data="totalEarnings"
                     icon-name="cash-outline"
                     caption="Zarobki"
                 />
                 <info-card
-                    data="45 mln. h"
+                    :data="viewTime"
                     icon-name="time-outline"
                     caption="Suma oglądalności"
                 />
+            </div>
+            <div class="contract">
+                <div class="text">
+                    <span>Twoja umowa</span>
+                </div>
+                <div class="card">
+                    <div class="section">
+                        <span class="caption">Ważność umowy:</span>
+                        <span class="important">do 21.12.2023r</span>
+                    </div>
+                    <div class="section">
+                        <span class="caption">Stawka za godzinę oglądalności:</span>
+                        <span class="important">0,03 zł</span>
+                    </div>
+                    <div class="section">
+                        <span class="caption">Procent otrzymywanych dotacji:</span>
+                        <span class="important">70%</span>
+                    </div>
+                    <div class="buttons">
+
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script setup>
+    const totalEarnings = ref('2 316 680 zł');
+    const views = ref('297.5 tys.');
+    const viewTime = ref('45 mln. h');
 
 </script>
 
@@ -82,5 +107,38 @@
     .brief-statistics {
         display: flex;
         gap: 1em;
+        margin-bottom: 3.2em;
+    }
+
+    .contract {
+        & > .text {
+            font-size: 2em;
+            margin-bottom: 0.6em;
+        }
+
+        & > .card {
+            background-color: $background600;
+            border: $primary500 0.2px solid;
+            border-radius: 12px;
+            box-shadow: 4px 4px 12px 0 $background800;
+            padding: 2em;
+            display: flex;
+            flex-direction: column;
+            gap: 0.8em;
+
+            & > .section {
+                display: flex;
+                gap: 0.6em;
+                align-items: flex-end;
+
+                & > .important {
+                    font-size: 1.6em;
+                }
+
+                & > .caption {
+                    font-size: 1.3em;
+                }
+            }
+        }
     }
 </style>
